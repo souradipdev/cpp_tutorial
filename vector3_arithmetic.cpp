62b1e2358feb77071d008ba9d29eec6b2567d234
souradipdev <22052939@kiit.ac.in>
2024-08-15 15:15:44 +0530

    Added useful comments

--- vector3_arithmetic.cpp
+++ vector3_arithmetic.cpp
@@ -24,35 +24,37 @@ int main() {
     cout << "vec1 and vec3 are equal." << endl;
   }
 
-  // Less-than comparison (<)
-  if (vec3 < vec1) {
-    cout << "vec3 is less than vec1." << endl;
-  } else {
-    cout << "vec3 is not less than vec1." << endl;
-  }
-
-  // Less-than-or-equal comparison (<=)
-  if (vec3 <= vec1) {
-    cout << "vec3 is less than or equal to vec1." << endl;
-  } else {
-    cout << "vec3 is not less than or equal to vec1." << endl;
-  }
-
-  // Greater-than comparison (>)
+  // Both vectors are compared element by element. If element of vec1 > vec3 at same index then vec1 is greater
+  // If size of vector1 > vector3 and if every element of vec1 > vec3 then vec1 is greater
+  // If a particular element of vec3 > vec1 then vec1 !> vec3 even if size of vec1 > vec3
   if (vec1 > vec3) {
     cout << "vec1 is greater than vec3." << endl;
   } else {
     cout << "vec1 is not greater than vec3." << endl;
   }
 
-  // Greater-than-or-equal comparison (>=)
+
+  if (vec3 <= vec1) {
+    cout << "vec3 is less than or equal to vec1." << endl;
+  } else {
+    cout << "vec3 is not less than or equal to vec1." << endl;
+  }
+
+  if (vec3 < vec1) {
+    cout << "vec3 is less than vec1." << endl;
+  } else {
+    cout << "vec3 is not less than vec1." << endl;
+  }
+
+
+
   if (vec1 >= vec2) {
     cout << "vec1 is greater than or equal to vec2." << endl;
   } else {
     cout << "vec1 is not greater than or equal to vec2." << endl;
   }
 
-  // Testing with a vector that has different order
+
   if (vec4 < vec1) {
     cout << "vec4 is less than vec1 (based on lexicographical order)." << endl;
   } else {
